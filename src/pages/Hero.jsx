6008522b5394f4ca32be190ba06aad7d4b4cab69
sifat2626 <ashrafulsifat26@gmail.com>
2024-11-10 16:02:14 +0600

    hero for xl

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,8 +1,31 @@
+import heroFood from "../assets/images/hero-food.jpeg"
+
 function Hero() {
   return (
-    <div className="h-screen">
-      <div className="">
-        <h2>hero</h2>
+    <div className="flex-1 flex flex-col justify-center">
+      <div className="flex py-4 relative ">
+        {/* Text Section */}
+        <div className="flex-1 flex flex-col justify-center items-start">
+          <h2 className="font-bebas font-bold text-[120px] tracking-[0.04em] leading-none text-white bg-gradient-to-r from-[#BD1F1700] to-[#BD1F1770] relative -mr-[15rem] z-10 py-3">
+            Taste the authentic <br /> Saudi cuisine
+          </h2>
+          <p className="font-robo text-2xl mt-4 font-normal mr-32">
+            Among the best Saudi chefs in the world, serving you something
+            beyond flavor.
+          </p>
+          <button className="mt-12 bg-[#FEBF00] text-black py-4 px-6 font-robo font-bold">
+            Explore Menu
+          </button>
+        </div>
+
+        {/* Image Section */}
+        <div className="flex-1 overflow-hidden relative">
+          <img
+            src={heroFood}
+            alt=""
+            className="object-cover w-full h-[650px] scale-x-[-1]"
+          />
+        </div>
       </div>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,15 @@ import Hero from "./Hero"
 
 function Home() {
   return (
-    <div className="bg-hero-bg bg-small ">
-      {/* <div className="absolute inset-0 bg-[#BD1F17] opacity-[88%]">
-      </div> */}
-      <div className="text-white font-bold text-2xl bg-red-500 opacity-[88%] custom-container">
+    <div className="relative h-screen">
+      {/* Background Image Layer */}
+      <div className="absolute inset-0 bg-hero-bg bg-small bg-center -z-10"></div>
+
+      {/* Overlay Layer */}
+      <div className="absolute inset-0 bg-[#BD1F17] opacity-[88%] "></div>
+
+      {/* Content Layer */}
+      <div className="relative z-10 text-white font-bold text-2xl custom-container flex flex-col h-full">
         <Navbar />
         <Hero />
       </div>
